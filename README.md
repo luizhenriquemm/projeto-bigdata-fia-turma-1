5acc0b7066f5e4fdecac26c611d065d73fd5a07d
Luiz Henrique <luiz.henrique.mm@hotmail.com>
2024-09-10 20:13:56 -0300

    Begin

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # projeto-bigdata-fia-turma-1
+
 Projeto de Big Data FIA - Turma 1
+
+Alunos:
+  - Rita de Cássia Rodrigues Fernandes Simoes
+  - Luiz Henrique de Melo Marcello
+  - Caio Santiago
+
+# Para executar o ambiente
+
+Todo o ambiente é a partir de uma versão reduzida do datalab usado em sala.
+
+Comando para subir o docker:
+```sh
+docker compose up -d
+```
+
---
+++ script.bat
@@ -0,0 +1,7 @@
+mkdir .\containers-data\postgres\volume\pg_notify
+mkdir .\containers-data\postgres\volume\pg_tblspc
+mkdir .\containers-data\postgres\volume\pg_twophase
+mkdir .\containers-data\postgres\volume\pg_commit_ts
+mkdir .\containers-data\postgres\volume\pg_snapshots
+mkdir .\containers-data\postgres\volume\pg_stat_tmp
+mkdir .\containers-data\postgres\volume\pg_logical\mappings
